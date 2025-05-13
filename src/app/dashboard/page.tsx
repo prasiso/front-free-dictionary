6338e1a66cc55f2cec08509be80d62d0569e4e84
Kelwin de Souza <kelwinsimoesdesouza@gmail.com>
2025-05-12 22:10:09 -0300

    feat: base end point

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+export default function Dashboard() {
+  return (
+    <div className="flex items-center justify-around h-screen w-screen">
+      <div>
+        <p> ELEMENTO DE PALAVRA UNICA </p>
+      </div>
+      <div>
+        <p> ELEMENTO DE PESQUISA </p>
+      </div>
+    </div>
+  );
+}
