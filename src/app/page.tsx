519914fc5afbe6cd21af5ea04002fe26c3d7644c
Kelwin de Souza <kelwinsimoesdesouza@gmail.com>
2025-05-15 10:16:57 -0300

    feat: auth guard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,4 @@
-'use client';
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { getUser } from "@/helper";
+"use client";
 export default function Home() {
-  const router = useRouter();
-  useEffect(() => {
-    const user = getUser();
-    if (user) router.replace("/dashboard");
-    router.replace("/signin");
-  }, [router]);
   return null;
 }
